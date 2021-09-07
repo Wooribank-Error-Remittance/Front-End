2d6a17ebb7b8ec86f841294ef9187eb518d26f7c
김승우 <seungwookim.dev@gmail.com>
2021-09-08 02:11:19 +0900

    [Add] 반환 요청 목록의 버튼 UI

--- lib/view/sent_return_request_list_page.dart
+++ lib/view/sent_return_request_list_page.dart
@@ -3,7 +3,7 @@ import 'dart:core';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:wooribank_error_remittance/view/first_page.dart';
+import 'package:slide_button/slide_button.dart';
 
 import 'account_list_page.dart';
 
@@ -13,6 +13,18 @@ class SentReturnRequestListPage extends StatefulWidget {
 }
 
 class _SentReturnRequestListState extends State<SentReturnRequestListPage> {
+  List<String> tempList = ['1', '2,' '3'];
+  var textEditingControllers = <TextEditingController>[];
+
+  @override
+  void initState() {
+    tempList.forEach((String str) {
+      var textEditingController = new TextEditingController();
+      textEditingControllers.add(textEditingController);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -46,7 +58,51 @@ class _SentReturnRequestListState extends State<SentReturnRequestListPage> {
           elevation: 0.0,
         ),
         body: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            Center(
+              child: Container(
+                width: 300,
+                height: 80,
+                child: SlideButton(
+                  backgroundChild: Center(child: Text('테스트')),
+                  backgroundColor: Color(0xFF3297F7),
+                  slidingChild: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFFFF766D),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRouteWithoutAnimation(
+                          builder: (context) => AccountListPage(),
+                        ),
+                      );
+                    },
+                    child: FittedBox(
+                      child: Text(
+                        textEditingControllers[1].text,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ),
+                  onButtonOpened: () {
+                    setState(() {
+                      textEditingControllers[1].text = "신고하기";
+                    });
+                  },
+                  onButtonClosed: () {
+                    WidgetsBinding.instance!
+                        .addPostFrameCallback((_) => setState(() {
+                              textEditingControllers[1].text = " ";
+                            }));
+                  },
+                  slideDirection: SlideDirection.LEFT,
+                  initialSliderPercentage: 0.14,
+                  confirmPercentage: 0.5,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       onWillPop: () async {

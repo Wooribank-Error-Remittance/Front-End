cd7c757f012d7b4f4eac1f5156d9a9ed4918d02e
김승우 <seungwookim.dev@gmail.com>
2021-09-13 15:15:42 +0900

    [Update] 계좌별 거래내역 폐이지 구현

--- lib/view/account_statement_page.dart
+++ lib/view/account_statement_page.dart
@@ -13,15 +13,8 @@ class AccountStatementPage extends StatefulWidget {
 }
 
 class _AccountStatementState extends State<AccountStatementPage> {
-  List<String> tempList = ['1', '2,' '3'];
-  var textEditingControllers = <TextEditingController>[];
-
   @override
   void initState() {
-    tempList.forEach((String str) {
-      var textEditingController = new TextEditingController();
-      textEditingControllers.add(textEditingController);
-    });
     super.initState();
   }
 
@@ -29,8 +22,10 @@ class _AccountStatementState extends State<AccountStatementPage> {
   Widget build(BuildContext context) {
     return WillPopScope(
       child: Scaffold(
+        backgroundColor: Color(0xFFFFFFFF),
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
+          shape: Border(bottom: BorderSide(color: Color(0xFF1C65B9), width: 0)),
           toolbarHeight: ScreenUtil().setHeight(40),
           leading: IconButton(
             onPressed: () {
@@ -60,7 +55,206 @@ class _AccountStatementState extends State<AccountStatementPage> {
           elevation: 0.0,
         ),
         body: Column(
-          children: <Widget>[],
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              color: Color(0xFF1C65B9),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: ScreenUtil().setHeight(12),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: ScreenUtil().setWidth(18),
+                      ),
+                      Text(
+                        'WON 통장',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: ScreenUtil().setSp(16),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: ScreenUtil().setWidth(18),
+                      ),
+                      Text(
+                        '1002-123-456789',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: ScreenUtil().setSp(16),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil().setHeight(25),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        '100,000 원',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: ScreenUtil().setSp(16),
+                        ),
+                      ),
+                      SizedBox(
+                        width: ScreenUtil().setWidth(18),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: ScreenUtil().setHeight(15),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: ScreenUtil().setHeight(40),
+              color: Color(0xFFF9F9F9),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.chevron_left,
+                      color: Color(0xFF5E5E5E),
+                      size: ScreenUtil().setSp(26),
+                    ),
+                  ),
+                  SizedBox(
+                    width: ScreenUtil().setWidth(30),
+                  ),
+                  Text(
+                    '2021.09',
+                    style: TextStyle(
+                        color: Color(0xFF5E5E5E),
+                        fontSize: ScreenUtil().setSp(14),
+                        fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(
+                    width: ScreenUtil().setWidth(30),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.chevron_right,
+                      color: Color(0xFF5E5E5E),
+                      size: ScreenUtil().setSp(26),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: 3,
+                itemBuilder: (BuildContext context, int index) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRouteWithoutAnimation(
+                          builder: (context) => AccountStatementPage(),
+                        ),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '05.23',
+                              style: TextStyle(
+                                color: Color(0xFF3A3A3A),
+                                fontSize: ScreenUtil().setSp(15),
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: ScreenUtil().setWidth(10),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '카카오법인택시',
+                              style: TextStyle(
+                                color: Color(0xFF000000),
+                                fontSize: ScreenUtil().setSp(15),
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            SizedBox(
+                              height: ScreenUtil().setHeight(5),
+                            ),
+                            Text(
+                              '체크카드',
+                              style: TextStyle(
+                                color: Color(0xFF747474),
+                                fontSize: ScreenUtil().setSp(14),
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              '+37,000 원',
+                              style: TextStyle(
+                                color: Color(0xFF3297F7),
+                                fontSize: ScreenUtil().setSp(14),
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: ScreenUtil().setWidth(6),
+                        )
+                      ],
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      splashFactory: NoSplash.splashFactory,
+                      shadowColor: Colors.transparent,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                      padding: EdgeInsets.fromLTRB(
+                          ScreenUtil().setSp(14),
+                          ScreenUtil().setSp(10),
+                          ScreenUtil().setSp(14),
+                          ScreenUtil().setSp(12)),
+                      fixedSize:
+                          Size(double.infinity, ScreenUtil().setHeight(70)),
+                      primary: Colors.white, // background
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
       onWillPop: () async {

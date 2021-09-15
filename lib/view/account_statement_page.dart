0cfdb9cf3b6921d0e0766e09debc2fd13c598e6f
김승우 <seungwookim.dev@gmail.com>
2021-09-15 15:23:42 +0900

    [Fix] 계좌 페이지 UI 수정

--- lib/view/account_statement_page.dart
+++ lib/view/account_statement_page.dart
@@ -162,184 +162,212 @@ class _AccountStatementState extends State<AccountStatementPage> {
                 shrinkWrap: true,
                 itemCount: 3,
                 itemBuilder: (BuildContext context, int index) {
-                  return ElevatedButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return Dialog(
-                            shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(25.0)),
-                            ),
-                            child: Container(
-                              height: ScreenUtil().setHeight(200),
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Color(0xFF3297F7), width: 2),
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(25.0),
+                  return Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return Dialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(25.0)),
                                 ),
-                              ),
-                              child: Column(
-                                children: [
-                                  Container(
-                                    height: ScreenUtil().setHeight(40),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          '착오송금액 반환 요청',
-                                          textAlign: TextAlign.center,
-                                          style: TextStyle(
-                                            color: Color(0xFF3297F7),
-                                            fontWeight: FontWeight.w600,
-                                            fontSize: ScreenUtil().setSp(17),
-                                          ),
-                                        ),
-                                      ],
+                                child: Container(
+                                  height: ScreenUtil().setHeight(200),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Color(0xFF3297F7), width: 2),
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(25.0),
                                     ),
                                   ),
-                                  Container(
-                                    color: Color(0xFF3297F7),
-                                    width: double.infinity,
-                                    height: ScreenUtil().setHeight(100),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          '해당 거래내역에 대해\n반환을 요청하시겠습니까?',
-                                          textAlign: TextAlign.center,
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.w600,
-                                            fontSize: ScreenUtil().setSp(16),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Container(
-                                    height: ScreenUtil().setHeight(50),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        SizedBox(
-                                          height:ScreenUtil().setHeight(50),
-                                          width:ScreenUtil().setWidth(120),
-                                          child: ElevatedButton(
-                                            onPressed: null,
-                                            child:
-                                                Image.asset('assets/button_accept.png'),
-                                            style: ElevatedButton.styleFrom(
-                                              onSurface: Colors.white,
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        height: ScreenUtil().setHeight(40),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              '착오송금액 반환 요청',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                color: Color(0xFF3297F7),
+                                                fontWeight: FontWeight.w600,
+                                                fontSize:
+                                                    ScreenUtil().setSp(17),
+                                              ),
                                             ),
-                                          ),
+                                          ],
                                         ),
-                                        SizedBox(
-                                          height:ScreenUtil().setHeight(50),
-                                          width:ScreenUtil().setWidth(120),
-                                          child: ElevatedButton(
-                                            onPressed: null,
-                                            child:
-                                            Image.asset('assets/button_decline.png'),
-                                            style: ElevatedButton.styleFrom(
-                                              onSurface: Colors.white,
+                                      ),
+                                      Container(
+                                        color: Color(0xFF3297F7),
+                                        width: double.infinity,
+                                        height: ScreenUtil().setHeight(100),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              '해당 거래내역에 대해\n반환을 요청하시겠습니까?',
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w600,
+                                                fontSize:
+                                                    ScreenUtil().setSp(16),
+                                              ),
                                             ),
-                                          ),
+                                          ],
                                         ),
-                                      ],
-                                    ),
+                                      ),
+                                      Container(
+                                        height: ScreenUtil().setHeight(50),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            SizedBox(
+                                              height:
+                                                  ScreenUtil().setHeight(50),
+                                              width: ScreenUtil().setWidth(120),
+                                              child: ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: Image.asset(
+                                                    'assets/button_accept.png'),
+                                                style: ElevatedButton.styleFrom(
+                                                  shadowColor:
+                                                      Colors.transparent,
+                                                  primary: Colors.white,
+                                                  onSurface: Colors.white,
+                                                ),
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height:
+                                                  ScreenUtil().setHeight(50),
+                                              width: ScreenUtil().setWidth(120),
+                                              child: ElevatedButton(
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: Image.asset(
+                                                    'assets/button_decline.png'),
+                                                style: ElevatedButton.styleFrom(
+                                                  shadowColor:
+                                                      Colors.transparent,
+                                                  primary: Colors.white,
+                                                  onSurface: Colors.white,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
-                              ),
-                            ),
+                                ),
+                              );
+                            },
                           );
                         },
-                      );
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            Text(
-                              '05.23',
-                              style: TextStyle(
-                                color: Color(0xFF3A3A3A),
-                                fontSize: ScreenUtil().setSp(15),
-                                fontWeight: FontWeight.w800,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          width: ScreenUtil().setWidth(10),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '카카오법인택시',
-                              style: TextStyle(
-                                color: Color(0xFF000000),
-                                fontSize: ScreenUtil().setSp(15),
-                                fontWeight: FontWeight.w400,
-                              ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: ScreenUtil().setHeight(3),
+                                ),
+                                Text(
+                                  '05.23',
+                                  style: TextStyle(
+                                    color: Color(0xFF3A3A3A),
+                                    fontSize: ScreenUtil().setSp(15),
+                                    fontWeight: FontWeight.w800,
+                                  ),
+                                ),
+                              ],
                             ),
                             SizedBox(
-                              height: ScreenUtil().setHeight(5),
+                              width: ScreenUtil().setWidth(10),
                             ),
-                            Text(
-                              '체크카드',
-                              style: TextStyle(
-                                color: Color(0xFF747474),
-                                fontSize: ScreenUtil().setSp(14),
-                                fontWeight: FontWeight.w400,
-                              ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: ScreenUtil().setHeight(4),
+                                ),
+                                Text(
+                                  '카카오법인택시',
+                                  style: TextStyle(
+                                    color: Color(0xFF000000),
+                                    fontSize: ScreenUtil().setSp(15),
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: ScreenUtil().setHeight(5),
+                                ),
+                                Text(
+                                  '체크카드',
+                                  style: TextStyle(
+                                    color: Color(0xFF747474),
+                                    fontSize: ScreenUtil().setSp(14),
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
                             ),
+                            Spacer(),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Text(
+                                  '+37,000 원',
+                                  style: TextStyle(
+                                    color: Color(0xFF3297F7),
+                                    fontSize: ScreenUtil().setSp(14),
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: ScreenUtil().setWidth(6),
+                            )
                           ],
                         ),
-                        Spacer(),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              '+37,000 원',
-                              style: TextStyle(
-                                color: Color(0xFF3297F7),
-                                fontSize: ScreenUtil().setSp(14),
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                          ],
+                        style: ElevatedButton.styleFrom(
+                          splashFactory: NoSplash.splashFactory,
+                          shadowColor: Colors.transparent,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(0),
+                          ),
+                          padding: EdgeInsets.fromLTRB(
+                              ScreenUtil().setSp(14),
+                              ScreenUtil().setSp(10),
+                              ScreenUtil().setSp(14),
+                              ScreenUtil().setSp(12)),
+                          fixedSize:
+                              Size(double.infinity, ScreenUtil().setHeight(70)),
+                          primary: Colors.white, // background
                         ),
-                        SizedBox(
-                          width: ScreenUtil().setWidth(6),
-                        )
-                      ],
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      splashFactory: NoSplash.splashFactory,
-                      shadowColor: Colors.transparent,
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0),
                       ),
-                      padding: EdgeInsets.fromLTRB(
-                          ScreenUtil().setSp(14),
-                          ScreenUtil().setSp(10),
-                          ScreenUtil().setSp(14),
-                          ScreenUtil().setSp(12)),
-                      fixedSize:
-                          Size(double.infinity, ScreenUtil().setHeight(70)),
-                      primary: Colors.white, // background
-                    ),
+                      Divider(
+                        height: 2,
+                        color: Color(0xFFE2DFDF),
+                      ),
+                    ],
                   );
                 },
               ),
